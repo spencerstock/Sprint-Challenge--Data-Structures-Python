033c08a466244bc699f49dbf6adfe395b62c0fdb
Spencer Stock <spencerstock@gmail.com>
2019-09-20 09:41:49 -0600

    Duplicate names

--- names/names.py
+++ names/names.py
@@ -1,20 +1,25 @@
 import time
+from binary_search_tree import BinarySearchTree
 
+
+bst = BinarySearchTree('n')
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
+for name in names_1:
+    bst.insert(name)
+
 
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+for name in names_2:
+    if bst.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
